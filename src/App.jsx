0f1f515bd2d87a6326f4881e206e94f61fc7564f
Denis <k1selevde@yandex.ru>
2021-06-19 17:51:33 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import "./App.scss"
+
+function App() {
+  return (
+    <div className="wrapper">
+        <div
+            className="window"
+        >
+            Text
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
